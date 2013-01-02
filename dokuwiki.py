94b9f21df848d8697ac0411939797c8f40cf10ee
diego <diego.ceccarelli@gmail.com>
2013-01-02 20:36:02 +0100

    added dokuwiki library

---
+++ dokuwiki.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+from jinja2 import nodes
+from jinja2.ext import Extension
+from jinja2 import Environment, Undefined
+import HTMLParser
+import re
+
+_htmlparser = HTMLParser.HTMLParser()
+unescape = _htmlparser.unescape
+
+
+if __name__ == "__main__":
+	input_file = sys.argv[1]
+	f = open(input_file)
+	output = subprocess.check_output(["dokuwiki/bin/render.php"],stdin=f)
+	output_file=input_file+".html"
+	o = open(output_file,"w")
+	o.write(output);
+	o.close()
+
+def convert(text):
+	global unescape
+	o = open("/tmp/doku","w")
+	
+	o.write(text)
+	o.close()
+	o = open("/tmp/doku")
+	output = subprocess.check_output(["dokuwiki/bin/render.php"],stdin=o)
+	output = re.sub('/dokuwiki/bin/lib/exe/detail.php?[^>]*media=(?P<file>[^">]+)', '/static/img/\g<file>', output)
+	output = re.sub('/dokuwiki/bin/lib/exe/fetch.php?[^>]*media=(?P<file>[^">]+)', '/static/img/\g<file>', output)
+	
+	return output
+	
+
+
+def doku(text):
+	o = open("log","w")
+	o.write(text)
+	o.close()
+	return convert(text)
+
+class DokuwikiExtension(Extension):
+	def __init__(self, environment):
+		super(DokuwikiExtension, self).__init__(environment)
+		environment.filters["doku"] = doku	
+		
