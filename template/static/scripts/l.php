4aae8865845a4d1f1c74de290892aebbdb3384a6
diego <diego.ceccarelli@gmail.com>
2013-06-10 15:22:50 +0200

    minor

--- template/static/scripts/l.php
+++ template/static/scripts/l.php
@@ -3,6 +3,7 @@
 //open up the log file
 $file = fopen('log.txt', 'a');
 $u = $_GET["p"];
+$r = $_GET["r"];
 
 // ip address of the visitor
 $ipadress = $_SERVER['REMOTE_ADDR'];
@@ -15,9 +16,10 @@ $date = date('d/F/Y h:i:s');
 // visitor's browser information
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
+
 // Opening the text file and writing the visitor's data
 
-fwrite($file, $ipadress.'	'.$hostname.'	'.$date.'	'.$u.'	'.$browser."\n");
+fwrite($file, $ipadress.'	'.$hostname.'	'.$date.'	'.$u.'	'.$browser.'	'.$r."\n");
 
 //and finial, close the log file
 fclose( $file );
